ea73bcb532121e30aed7a567b38185b91972e9a8
jiink <jacob.j.carlson03@gmail.com>
2023-05-18 21:07:21 -0700

    (Python) Implemented reveal algorithm and game over

--- python/pysweeper.py
+++ python/pysweeper.py
@@ -5,6 +5,8 @@ import random
 
 move_counter = 0
 
+game_over = False
+
 # The cursor is controller by arrow keys and is used to select cells.
 cursor_x = 0
 cursor_y = 0
@@ -14,15 +16,24 @@ grid_rows = 10
 grid_cols = 10
 
 # Cell content meanings:
-cell_empty = 0
+cell_empty = 0 # This means "there is no mine here nor around here"
 cell_mine = -1
 
 # The data structure for the playfield/grid gamestate.
 # It's full of integers. Each number tells the cell's contents.
 grid = [[0 for _ in range(grid_rows)] for _ in range(grid_cols)]
+revealed = [[False for _ in range(grid_rows)] for _ in range(grid_cols)] # Boolean: True if revealed, False if not
 
 # Show grid contents
 
+def print_revealed_grid(grid):
+    for row in grid:
+        for element in row:
+            if element == True: # If revealed
+                print(" ", end="")
+            else: 
+                print("?", end="")
+        print()
 
 def print_grid(grid):
     for row in grid:
@@ -37,12 +48,42 @@ def print_grid(grid):
 
 
 def dig(x, y):
-    # Unimplemented
+    row = y
+    col = x
+    # Only dig if the cell is not revealed yet
+    if revealed[row][col]:
+        return
     print("Dug...")
+    # Reveal this cell always
+    revealed[row][col] = True
+    if grid[row][col] == cell_empty:
+        # ... and the ones around it
+        # Define the relative positions of the neighboring cells
+        neighbors = [
+            (-1, -1), (-1, 0), (-1, 1),
+            (0, -1),           (0, 1),
+            (1, -1),  (1, 0),  (1, 1)
+        ]
+
+        # Iterate over the neighboring cells
+        for dr, dc in neighbors:
+            new_row, new_col = row + dr, col + dc
+
+            # Check if the new position is within the grid boundaries
+            if 0 <= new_row < len(grid) and 0 <= new_col < len(grid[0]):
+                # Look for 0-value cells around it and call dig(x,y) on those
+                # too, to propagate the digging.
+                if grid[new_row][new_col] == 0:
+                    dig(new_col, new_row)
+                else:
+                    revealed[new_row][new_col] = True
+
+    elif grid[row][col] == cell_mine:
+        print("You hit a mine!")
+        print("💀//////// Game over! ////////💀")
+
 
 # Move around cursor with WASD, pick a cell to dig with space.
-
-
 def handle_input():
     global cursor_x, cursor_y  # Global variables this function modifies
 
@@ -74,6 +115,8 @@ def display_game_state():
     print("Move #" + str(move_counter))
     print_grid(grid)
     print("Cursor is at: " + str(cursor_x) + ", " + str(cursor_y))
+    print("Revealed cells:")
+    print_revealed_grid(revealed)
     print("--------------------")
 
 # Function for setting up Mines
@@ -124,14 +167,15 @@ def create_number_cells():
                 # Count neighboring mines and write the number
                 grid[row][col] = get_num_of_neighboring_mines(row, col)
 
-
 def initialize_grid():
     create_mines()
     create_number_cells()
 
 # Game loop -------------------------------------------------------------
+print("Type W, A, S, D, or Space and hit Enter.")
 initialize_grid()
-while True:
+while not game_over:
     move_counter += 1
     handle_input()
     display_game_state()
+print("ℹ Goodbye.")
